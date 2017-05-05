58eba3b5fbc63f476752df47c533466bedc22620
yiminghe <yiminghe@gmail.com>
2017-05-05 13:01:21 +0800

    Updates

--- examples/demo.js
+++ examples/demo.js
@@ -1,63 +1,61 @@
-webpackJsonp([0],[
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+webpackJsonp([2],{
 
-	module.exports = __webpack_require__(1);
+/***/ 122:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_rc_touchable__ = __webpack_require__(49);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_rc_touchable___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_rc_touchable__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react__ = __webpack_require__(48);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dom__ = __webpack_require__(40);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react_dom__);
+/* tslint:disable:no-console */
+
+
+
+var style = '\n.active {\n  background: red;\n}\n.x {\n display:inline-block;\n width:100px;\n height:100px;\n border:1px solid yellow;\n}\n.x:active {\n  background: red;\n}\n';
+var Test = __WEBPACK_IMPORTED_MODULE_1_react___default.a.createClass({
+    displayName: 'Test',
+    componentWillMount: function componentWillMount() {
+        window.log = this.log;
+    },
+    onPress: function onPress(e) {
+        this.log('onPress', e);
+    },
+    onLongPress: function onLongPress(e) {
+        this.log('onLongPress', e);
+    },
+    log: function log(m) {
+        this.refs.log.innerHTML += '<p>' + m + ':' + Date.now() + '</p>';
+        this.refs.log.scrollTop = this.refs.log.scrollHeight;
+    },
+    render: function render() {
+        var _this = this;
+
+        return __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("div", { style: { margin: '20px' } }, __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("div", { ref: "log", style: { height: 100, overflow: 'auto', margin: 10 } }), __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("style", { dangerouslySetInnerHTML: { __html: style } }), __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_0_rc_touchable___default.a, { activeStyle: { border: '1px solid yellow', padding: 5 }, activeClassName: "active", onPress: this.onPress, onLongPress: this.onLongPress }, __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("div", { style: {
+                width: 100,
+                height: 100,
+                border: '1px solid red',
+                boxSizing: 'border-box',
+                WebkitUserSelect: 'none'
+            } }, "click")), __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("br", null), __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("br", null), __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("div", { tabIndex: 0, className: "x", onClick: function onClick() {
+                _this.log('onClick');
+            } }, "click"));
+    }
+});
+__WEBPACK_IMPORTED_MODULE_2_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(Test, null), document.getElementById('__react-content'));
 
 /***/ }),
-/* 1 */
+
+/***/ 268:
 /***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	var _rcTouchable = __webpack_require__(2);
-	
-	var _rcTouchable2 = _interopRequireDefault(_rcTouchable);
-	
-	var _react = __webpack_require__(4);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(39);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-	
-	var style = '\n.active {\n  background: red;\n}\n.x {\n display:inline-block;\n width:100px;\n height:100px;\n border:1px solid yellow;\n}\n.x:active {\n  background: red;\n}\n'; /* tslint:disable:no-console */
-	
-	var Test = _react2['default'].createClass({
-	    displayName: 'Test',
-	    componentWillMount: function componentWillMount() {
-	        window.log = this.log;
-	    },
-	    onPress: function onPress(e) {
-	        this.log('onPress', e);
-	    },
-	    onLongPress: function onLongPress(e) {
-	        this.log('onLongPress', e);
-	    },
-	    log: function log(m) {
-	        this.refs.log.innerHTML += '<p>' + m + ':' + Date.now() + '</p>';
-	        this.refs.log.scrollTop = this.refs.log.scrollHeight;
-	    },
-	    render: function render() {
-	        var _this = this;
-	
-	        return _react2['default'].createElement("div", { style: { margin: '20px' } }, _react2['default'].createElement("div", { ref: "log", style: { height: 100, overflow: 'auto', margin: 10 } }), _react2['default'].createElement("style", { dangerouslySetInnerHTML: { __html: style } }), _react2['default'].createElement(_rcTouchable2['default'], { activeStyle: { border: '1px solid yellow', padding: 5 }, activeClassName: "active", onPress: this.onPress, onLongPress: this.onLongPress }, _react2['default'].createElement("div", { style: {
-	                width: 100,
-	                height: 100,
-	                border: '1px solid red',
-	                boxSizing: 'border-box',
-	                WebkitUserSelect: 'none'
-	            } }, "click")), _react2['default'].createElement("br", null), _react2['default'].createElement("br", null), _react2['default'].createElement("div", { tabIndex: 0, className: "x", onClick: function onClick() {
-	                _this.log('onClick');
-	            } }, "click"));
-	    }
-	});
-	_reactDom2['default'].render(_react2['default'].createElement(Test, null), document.getElementById('__react-content'));
+module.exports = __webpack_require__(122);
+
 
 /***/ })
-]);
+
+},[268]);
 //# sourceMappingURL=demo.js.map
--- examples/disable-outer.js
+++ examples/disable-outer.js
@@ -1,76 +1,71 @@
-webpackJsonp([2],{
+webpackJsonp([0],{
 
-/***/ 0:
-/***/ (function(module, exports, __webpack_require__) {
+/***/ 124:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-	module.exports = __webpack_require__(186);
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_rc_touchable__ = __webpack_require__(49);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_rc_touchable___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_rc_touchable__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react__ = __webpack_require__(48);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dom__ = __webpack_require__(40);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react_dom__);
+/* tslint:disable:no-console */
 
+
+
+var style = '\n.foo-button {\n  display: inline-block;\n  width: 200px;\n  height: 100px;\n  border: 1px solid #ccc;\n  color: blue;\n}\n.foo-button.disabled {\n  background-color: black;\n}\n';
+var Test = __WEBPACK_IMPORTED_MODULE_1_react___default.a.createClass({
+    displayName: 'Test',
+    getInitialState: function getInitialState() {
+        return {
+            value: 1,
+            max: 3,
+            min: 1
+        };
+    },
+    onPressPrev: function onPressPrev(e) {
+        console.log('onPressPrev', e);
+        this.prev();
+    },
+    onPressNext: function onPressNext(e) {
+        console.log('onPressNext', e);
+        this.next();
+    },
+    onLongPress: function onLongPress(e) {
+        console.log('onLongPress', e);
+    },
+    prev: function prev() {
+        this.setState({
+            value: this.state.value - 1
+        });
+    },
+    next: function next() {
+        this.setState({
+            value: this.state.value + 1
+        });
+    },
+    render: function render() {
+        var _state = this.state,
+            value = _state.value,
+            min = _state.min,
+            max = _state.max;
+
+        return __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("div", { style: { margin: '20px' } }, __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("style", { dangerouslySetInnerHTML: { __html: style } }), __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_0_rc_touchable___default.a, { activeStyle: { border: '1px solid yellow', padding: 5 }, activeClassName: "active", onPress: this.onPressPrev, onLongPress: this.onLongPress, disabled: value === min }, __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("button", { className: 'foo-button ' + (value === min ? 'disabled' : ''), role: "button" }, "prev page")), __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("div", null, "Now page: ", value, " "), __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_0_rc_touchable___default.a, { activeStyle: { border: '1px solid yellow', padding: 5 }, activeClassName: "active", onPress: this.onPressNext, onLongPress: this.onLongPress, disabled: value === max }, __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement("button", { className: 'foo-button ' + (value === max ? 'disabled' : ''), role: "button" }, "next page")));
+    }
+});
+__WEBPACK_IMPORTED_MODULE_2_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(Test, null), document.getElementById('__react-content'));
 
 /***/ }),
 
-/***/ 186:
+/***/ 270:
 /***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	var _rcTouchable = __webpack_require__(2);
-	
-	var _rcTouchable2 = _interopRequireDefault(_rcTouchable);
-	
-	var _react = __webpack_require__(4);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(39);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-	
-	var style = '\n.foo-button {\n  display: inline-block;\n  width: 200px;\n  height: 100px;\n  border: 1px solid #ccc;\n  color: blue;\n}\n.foo-button.disabled {\n  background-color: black;\n}\n'; /* tslint:disable:no-console */
-	
-	var Test = _react2['default'].createClass({
-	    displayName: 'Test',
-	    getInitialState: function getInitialState() {
-	        return {
-	            value: 1,
-	            max: 3,
-	            min: 1
-	        };
-	    },
-	    onPressPrev: function onPressPrev(e) {
-	        console.log('onPressPrev', e);
-	        this.prev();
-	    },
-	    onPressNext: function onPressNext(e) {
-	        console.log('onPressNext', e);
-	        this.next();
-	    },
-	    onLongPress: function onLongPress(e) {
-	        console.log('onLongPress', e);
-	    },
-	    prev: function prev() {
-	        this.setState({
-	            value: this.state.value - 1
-	        });
-	    },
-	    next: function next() {
-	        this.setState({
-	            value: this.state.value + 1
-	        });
-	    },
-	    render: function render() {
-	        var _state = this.state,
-	            value = _state.value,
-	            min = _state.min,
-	            max = _state.max;
-	
-	        return _react2['default'].createElement("div", { style: { margin: '20px' } }, _react2['default'].createElement("style", { dangerouslySetInnerHTML: { __html: style } }), _react2['default'].createElement(_rcTouchable2['default'], { activeStyle: { border: '1px solid yellow', padding: 5 }, activeClassName: "active", onPress: this.onPressPrev, onLongPress: this.onLongPress, disabled: value === min }, _react2['default'].createElement("button", { className: 'foo-button ' + (value === min ? 'disabled' : ''), role: "button" }, "prev page")), _react2['default'].createElement("div", null, "Now page: ", value, " "), _react2['default'].createElement(_rcTouchable2['default'], { activeStyle: { border: '1px solid yellow', padding: 5 }, activeClassName: "active", onPress: this.onPressNext, onLongPress: this.onLongPress, disabled: value === max }, _react2['default'].createElement("button", { className: 'foo-button ' + (value === max ? 'disabled' : ''), role: "button" }, "next page")));
-	    }
-	});
-	_reactDom2['default'].render(_react2['default'].createElement(Test, null), document.getElementById('__react-content'));
+module.exports = __webpack_require__(124);
+
 
 /***/ })
 
-});
+},[270]);
 //# sourceMappingURL=disable-outer.js.map
